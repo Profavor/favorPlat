b3b91baf9cd8c3dbcf76d832bfc5b44c71790b4d
profavor <profavor@naver.com>
2019-01-12 23:11:08 +0900

    6d4a44e

---
+++ _nuxt/e9a05321e44fd32c25b8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{337:function(e,n,t){"use strict";t.r(n);var i={created:function(){this.$router.push("/helper/dashboard")}},r=t(12),s=Object(r.a)(i,function(){var e=this.$createElement;return(this._self._c||e)("div")},[],!1,null,null,null);s.options.__file="index.vue";n.default=s.exports}}]);
